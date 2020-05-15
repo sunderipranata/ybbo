93d6933b544c4a6ae8eef4f50e32ce37c38228a2
Sunderi Pranata <sunderip128@gmail.com>
2020-05-16 04:59:07 +0700

    try use yml again

--- api/models/business.rb
+++ api/models/business.rb
@@ -4,15 +4,15 @@ class Business
   include Mongoid::Document
   field :name, type: String
 
-  def initialize
-    Mongoid.configure do |config|
-      config.clients.default = {
-        hosts: ["#{ENV['MONGOID_HOST']}"],
-        database: "#{ENV['MONGOID_CLUSTER']}",
-      }
-    end
+  # def initialize
+  #   Mongoid.configure do |config|
+  #     config.clients.default = {
+  #       hosts: ["#{ENV['MONGOID_HOST']}"],
+  #       database: "#{ENV['MONGOID_CLUSTER']}",
+  #     }
+  #   end
 
-    config.log_level = :warn
-  end
+  #   config.log_level = :warn
+  # end
 
 end
