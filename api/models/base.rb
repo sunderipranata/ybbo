c9f62a88aa8889907ec5c6033933d37da9f21bda
Sunderi Pranata <sunderip128@gmail.com>
2020-05-16 03:31:01 +0700

    test

---
+++ api/models/base.rb
@@ -0,0 +1,18 @@
+# class Base
+#   def initialize
+#   end
+
+#   def config!
+#     Her::API.setup url: "https://api.example.com" do |c|
+#       # Request
+#       c.use Faraday::Request::UrlEncoded
+
+#       # Response
+#       c.use Her::Middleware::DefaultParseJSON
+
+#       # Adapter
+#       c.use Faraday::Adapter::NetHttp
+#     end
+#   end
+
+# end
