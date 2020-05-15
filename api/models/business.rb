e1169cb64ba4246a78a39ea02f810e52f71b2d96
Sunderi Pranata <sunderip128@gmail.com>
2020-05-16 05:04:40 +0700

    this should work.. maybe

--- api/models/business.rb
+++ api/models/business.rb
@@ -1,18 +1,14 @@
 require 'mongoid'
 
+Mongoid.configure do |config|
+  config.clients.default = {
+    hosts: ["#{ENV['MONGOID_HOST']}"],
+    database: "#{ENV['MONGOID_CLUSTER']}",
+  }
+  config.log_level = :warn
+end
+
 class Business
   include Mongoid::Document
   field :name, type: String
-
-  # def initialize
-  #   Mongoid.configure do |config|
-  #     config.clients.default = {
-  #       hosts: ["#{ENV['MONGOID_HOST']}"],
-  #       database: "#{ENV['MONGOID_CLUSTER']}",
-  #     }
-  #   end
-
-  #   config.log_level = :warn
-  # end
-
 end
