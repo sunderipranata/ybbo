fadf69813fec12708660c945182f85cc4d61e52c
Sunderi Pranata <sunderip128@gmail.com>
2020-05-16 05:30:52 +0700

    add logs

--- api/models/business.rb
+++ api/models/business.rb
@@ -1,6 +1,9 @@
 require 'mongoid'
 
 Mongoid.configure do |config|
+  puts "Hello world"
+  puts "#{ENV['MONGOID_HOST']}"
+  puts "#{ENV['MONGOID_CLUSTER']}"
   config.clients.default = {
     hosts: ["#{ENV['MONGOID_HOST']}"],
     database: "#{ENV['MONGOID_CLUSTER']}",
