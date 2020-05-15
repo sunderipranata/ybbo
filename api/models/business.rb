41c68173684b1252244e228fc8f0ff159ec66df2
Sunderi Pranata <sunderip128@gmail.com>
2020-05-16 04:54:40 +0700

    mongoid yml does not working

--- api/models/business.rb
+++ api/models/business.rb
@@ -1,6 +1,17 @@
 require 'mongoid'
 
 class Business
- include Mongoid::Document
- field :name, type: String
+  include Mongoid::Document
+  field :name, type: String
+
+  def initialize
+    Mongoid.configure do |config|
+      config.clients.default = {
+      hosts: ["#{ENV['MONGOID_HOST']}"],
+      database: "#{ENV['MONGOID_CLUSTER']}",
+    }
+
+    config.log_level = :warn
+  end
+
 end
