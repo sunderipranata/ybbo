bf04631754afd0f69d3fe1e3e913f0e27ed23268
Sunderi Pranata <sunderip128@gmail.com>
2020-05-17 03:45:56 +0700

    cowsay debugger

--- api/business.rb
+++ api/business.rb
@@ -7,19 +7,26 @@ require_relative '../api_helper/initializers/mongoid'
 require_relative '../api_helper/initializers/response'
 require_relative '../api_helper/initializers/errors'
 
+# to be deleted
+require 'cowsay'
+
 Handler = Proc.new do |req, res|
   res['Content-Type'] = 'application/json'
+  debugger = ""
   begin
     case req.request_method
     when "GET"
       id = req.query['id'] || ""
       limit = req.query['limit'].to_i || 10
       offset = req.query['offset'] ? BSON::ObjectId(offset).to_time : Time.now
-
+      debugger << "checkpoint 1"
       if id.blank?
+        debugger << "checkpoint 2"
         business = Business.where(:created_at.lte => offset).limit(limit)
+        debugger << "checkpoint 3 #{business.count}"
         res.status = 200
         res.body = JSON::Response.paginate(business, limit, res.status)
+        debugger << "checkpoint 4 #{res.body}"
       else
         business = Business.find_by(id: id)
         res.status = 200
@@ -36,6 +43,6 @@ Handler = Proc.new do |req, res|
   rescue StandardError => e
     res.status = 200
     res['Content-Type'] = 'text/plain'
-    res.body = Cowsay.say("#{req.request_method} resulting in error #{e.message}", 'cow')
+    res.body = Cowsay.say("#{req.request_method} resulting in error #{e.message} -- debug: #{debugger}", 'cow')
   end
 end
