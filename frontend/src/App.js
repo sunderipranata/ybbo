3e228084249dc09712496add535d1b5c7a786a5a
Devin Ryan Riota <devinryanriota@gmail.com>
2020-05-17 17:40:25 +0700

    setup routings

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import { HOME_PATH } from './routes'
+import logo from './logo.svg';
+import './App.scss';
+
+import Home from './routes/Home'
+
+class App extends React.Component {
+  render = () => {
+    const isComingSoon = true
+    if(isComingSoon) {
+      return this.renderComingSoon()
+    } else {
+      return this.routes()
+    }
+  }
+  
+  routes = () => {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path={HOME_PATH} component={Home} />
+        </Switch>
+      </Router>
+    )
+  }
+  
+  renderComingSoon = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Yuk Bantu Bisnis Online - Coming Soon!
+          </p>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ frontend/src/routes/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { withRouter } from 'react-router-dom'
+
+class Home extends React.Component {
+  render = () => {
+    return (
+      <div>
+        Homee!
+      </div>
+    )
+  }
+}
+
+export default withRouter(Home)
