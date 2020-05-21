68aa46cff584242e982e5172671c374cd1f0f897
nelsonwija <iamnewie@gmail.com>
2020-05-21 21:46:43 +0700

    [preview] Update test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Yuk Bantu Bisnis Online/i);
+  const linkElement = getByText(/Homee!/i);
   expect(linkElement).toBeInTheDocument();
 });
