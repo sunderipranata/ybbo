045037e4d51579bc92723a6b767b16250bcd1a6d
Sunderi Pranata <sunderip128@gmail.com>
2020-05-23 18:45:51 +0700

    use .env for mongo creds

--- app/scripts/seed.rb
+++ app/scripts/seed.rb
@@ -1,10 +1,51 @@
 
 i=65
 b = Business.new
-# b = Business.find_by()
-
-b.name = "Toko Lapak #{i.chr}"
+b.name = "Toko Lapak Food #{i.chr}"
 b.category = "food_and_beverage"
+b.location = 'Kemang, Jakarta Barat'
+b.description = "Jual murah ... ... ... .. ... .. .."
+b.instructions = "silakan order lewat go food!"
+b.assets_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.icon_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.thumbnail_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.push(pictures_url: "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg")
+b.validate!
+b.save
+i+=1
+
+
+b = Business.new
+b.name = "Toko Lapak Fashion #{i.chr}"
+b.category = "fashion"
+b.location = 'Kemang, Jakarta Barat'
+b.description = "Jual murah ... ... ... .. ... .. .."
+b.instructions = "silakan order lewat go food!"
+b.assets_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.icon_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.thumbnail_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.push(pictures_url: "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg")
+b.validate!
+b.save
+i+=1
+
+b = Business.new
+b.name = "Toko Lapak Fashion #{i.chr}"
+b.category = "fashion"
+b.location = 'Kemang, Jakarta Barat'
+b.description = "Jual murah ... ... ... .. ... .. .."
+b.instructions = "silakan order lewat go food!"
+b.assets_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.icon_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.thumbnail_url = "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg"
+b.push(pictures_url: "https://youngmenshealthsite.org/wp-content/uploads/2015/05/iStock_000016515039Small-150x150.jpg")
+b.validate!
+b.save
+i+=1
+
+b = Business.new
+b.name = "Toko Lapak Beauty #{i.chr}"
+b.category = "beauty"
 b.location = 'Kemang, Jakarta Barat'
 b.description = "Jual murah ... ... ... .. ... .. .."
 b.instructions = "silakan order lewat go food!"
