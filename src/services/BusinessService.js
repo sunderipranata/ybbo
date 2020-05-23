5f3c6babdde2c08af6f8de397ac0c946c7449167
Devin Ryan Riota <devinryanriota@gmail.com>
2020-05-23 21:04:02 +0700

    home page fe-be integration (#20)
    * add get business simplified
    
    * CORS in vercel.json
    
    * [preview]
    
    * service, parsing API data
    
    * calculate page
    
    * detect device and adjust page limit
    
    * yarn
    
    * [preview]
    
    * refactor component methods
    
    * update pages count
    
    * render pages, loading, display
    
    * todo
    
    * yarn
    
    * [preview]
    
    * add name field
    
    * pagination wip
    
    * fix fetch and loading
    
    * working pagination
    
    * complete TODO
    
    * [preview]
    
    * wip
    
    * category changes desktop
    
    * category changes mobile
    
    * [preview]
    
    * const
    
    * fix page
    
    * [preview]
    
    * remove log
    
    * update render pagination
    
    * update page size desktop
    
    * [preview]
    
    * message if no products
    
    * [preview]

--- src/services/BusinessService.js
+++ src/services/BusinessService.js
@@ -1,15 +1,23 @@
 import axios from 'axios'
 
-// const BASE_URL = 'localhost'
-// const STH_PATH = '/business'
+const BASE_URL = 'https://ybbo-nqyz55lkg.now.sh/api'
+// const BASE_URL = 'https://yukbantubisnis.online/api'
+const BUSINESS_SIMPLIFIED = '/businesses'
 let BusinessService = {};
 
-BusinessService.get = async (id, callback) => {
-  await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    .then((response) => {
-      console.log('response', response)
-      callback(response)
-    })
+BusinessService.getSimplifiedWithLimitOffset = (limit, offset, category, callback) => {
+  const PATH = BASE_URL + BUSINESS_SIMPLIFIED
+  const categoryOption = category === 'all' ? null : category
+  axios.get(PATH, {
+    params: {
+      limit: limit,
+      offset: offset,
+      category: categoryOption
+    }
+  })
+  .then((response) => {
+    callback(response)
+  })
 }
 
 export default BusinessService;
