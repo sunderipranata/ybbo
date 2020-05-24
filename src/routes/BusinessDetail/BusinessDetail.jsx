19246e4513290c356998259473651769dcb55d88
Devin Ryan Riota <devinryanriota@gmail.com>
2020-05-24 13:41:04 +0700

    render functions

--- src/routes/BusinessDetail/BusinessDetail.jsx
+++ src/routes/BusinessDetail/BusinessDetail.jsx
@@ -76,164 +76,182 @@ class BusinessDetail extends Component {
     }
   }
 
-  render() {
-    const { isLoading, showForm } = this.state
+  renderLoadingDesktop = () => {
+    return (
+      <Fragment>
+        <main className="container container__business clearfix">
+          <div className="content__main">
+            <section name="Pengantar Bisnis">
+              <BusinessDetailIntro isLoading />
+            </section>
+            <section name="Tentang Bisnis" className="bd-card">
+              <div className="bd-card__content">
+                <BusinessDetailAbout isLoading />
+              </div>
+            </section>
+            <section name="Cara Mendukung" className="bd-card">
+              <BusinessDetailHowTo isLoading />
+            </section>
+            <section name="Galeri" className="bd-card">
+              <BusinessDetailGallery isLoading />
+            </section>
+            <section name="Pendukung" className="bd-card">
+              <div className="bd-card__content">
+                <BusinessDetailBackers isLoading  />
+              </div>
+            </section>
+          </div>
+          <div className="content__sidebar">
+            <BusinessDetailForm isLoading />
+          </div>
+        </main>
+        <Recomendations isLoading />
+      </Fragment>
+    )
+  }
+
+  renderLoadingMobile = () => {
+    return (
+      <Fragment>
+        <main className="container--mobile">
+          <section name="Pengantar Bisnis" className="bd-card__content">
+            <BusinessDetailIntro isLoading />
+          </section>
+          <section name="Tentang Bisnis" className="bd-card__content">
+            <BusinessDetailAbout isLoading />
+          </section>
+          <hr className="divider" />
+          <section name="Cara Mendukung">
+            <BusinessDetailHowTo isLoading  />
+          </section>
+          <hr className="divider" />
+          <section name="Galeri">
+            <BusinessDetailGallery isLoading />
+          </section>
+          <hr className="divider" />
+          <section name="Pendukung" className="bd-card__content" style={{ marginBottom: '72px' }}>
+            <BusinessDetailBackers isLoading />
+          </section>
+        </main>
+      </Fragment>
+    )
+  }
+
+  renderDetailDesktop = () => {
+    return (
+      <Fragment>
+        <main className="container container__business clearfix">
+          <div className="content__main">
+            <section name="Pengantar Bisnis">
+              <BusinessDetailIntro 
+                img="https://via.placeholder.com/120x120" 
+                title="Nama Bisnis" 
+                category="Kategori"
+                location="Lokasi" />
+            </section>
+            <section name="Tentang Bisnis" className="bd-card">
+              <div className="bd-card__content">
+                <BusinessDetailAbout
+                  title="Nama Bisnis" 
+                  desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                  when an unknown printer took a galley of type and scrambled it to make a type book."
+                  instagram="sylviestephanies"
+                  tokopedia="houseoforganix"
+                />
+              </div>
+            </section>
+            <section name="Cara Mendukung" className="bd-card clearfix">
+              <BusinessDetailHowTo title="Nama Bisnis"  />
+            </section>
+            <section name="Galeri" className="bd-card">
+              <SimpleReactLightbox>
+                <BusinessDetailGallery />
+              </SimpleReactLightbox>
+            </section>
+            <section ref={ (ref) => this.backersRef=ref } name="Pendukung" className="bd-card">
+              <div className="bd-card__content">
+                <BusinessDetailBackers title="Nama Bisnis"  />
+              </div>
+            </section>
+          </div>
+          <div className="content__sidebar">
+            <BusinessDetailForm scrollToBackers={this.scrollToBackers} />
+          </div>
+        </main>
+        <Recomendations />
+      </Fragment>
+    )
+  }
+
+  renderDetailMobile = () => {
+    const { showForm } = this.state
+
+    return (
+      <Fragment>
+        <main className="container--mobile">
+          <section name="Pengantar Bisnis" className="bd-card__content">
+            <BusinessDetailIntro 
+              img="https://via.placeholder.com/120x120" 
+              title="Nama Bisnis" 
+              category="Kategori"
+              location="Lokasi" />
+          </section>
+          <section name="Tentang Bisnis" className="bd-card__content">
+            <BusinessDetailAbout
+              title="Nama Bisnis" 
+              desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type book."
+              instagram="sylviestephanies"
+              tokopedia="houseoforganix"
+            />
+          </section>
+          <hr className="divider" />
+          <section name="Cara Mendukung">
+            <BusinessDetailHowTo title="Nama Bisnis"  />
+          </section>
+          <hr className="divider" />
+          <section name="Galeri">
+            <SimpleReactLightbox>
+              <BusinessDetailGallery />
+            </SimpleReactLightbox>
+          </section>
+          <hr className="divider" />
+          <section name="Pendukung" className="bd-card__content" style={{ marginBottom: '72px' }}>
+            <BusinessDetailBackers title="Nama Bisnis"  />
+          </section>
+        </main>
+        <div className="bd-bottom">
+          <div className="bd-bottom__content">
+            <button className={ClassNames('button button--main')} onClick={this.toggleForm}>Dukung Nama Bisnis</button>
+          </div>
+        </div>
+        <BottomSheet title="Dukung Nama Bisnis"
+          onClose={this.toggleForm}
+          display={showForm}
+        >
+          <div className="bd-bottom__form">
+          <BusinessDetailForm />
+          </div>
+        </BottomSheet>
+      </Fragment>
+    ) 
+  }
+
+  render = () => {
+    const { isLoading } = this.state
 
     return (
       <Fragment>
         <Desktop>
           <Header />
-          { isLoading ?
-            <Fragment>
-              <main className="container container__business clearfix">
-                <div className="content__main">
-                  <section name="Pengantar Bisnis">
-                    <BusinessDetailIntro isLoading />
-                  </section>
-                  <section name="Tentang Bisnis" className="bd-card">
-                    <div className="bd-card__content">
-                      <BusinessDetailAbout isLoading />
-                    </div>
-                  </section>
-                  <section name="Cara Mendukung" className="bd-card">
-                    <BusinessDetailHowTo isLoading />
-                  </section>
-                  <section name="Galeri" className="bd-card">
-                    <BusinessDetailGallery isLoading />
-                  </section>
-                  <section name="Pendukung" className="bd-card">
-                    <div className="bd-card__content">
-                      <BusinessDetailBackers isLoading  />
-                    </div>
-                  </section>
-                </div>
-                <div className="content__sidebar">
-                  <BusinessDetailForm isLoading />
-                </div>
-              </main>
-              <Recomendations isLoading />
-            </Fragment>
-            :
-            <Fragment>
-              <main className="container container__business clearfix">
-                <div className="content__main">
-                  <section name="Pengantar Bisnis">
-                    <BusinessDetailIntro 
-                      img="https://via.placeholder.com/120x120" 
-                      title="Nama Bisnis" 
-                      category="Kategori"
-                      location="Lokasi" />
-                  </section>
-                  <section name="Tentang Bisnis" className="bd-card">
-                    <div className="bd-card__content">
-                      <BusinessDetailAbout
-                        title="Nama Bisnis" 
-                        desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type book."
-                        instagram="sylviestephanies"
-                        tokopedia="houseoforganix"
-                      />
-                    </div>
-                  </section>
-                  <section name="Cara Mendukung" className="bd-card clearfix">
-                    <BusinessDetailHowTo title="Nama Bisnis"  />
-                  </section>
-                  <section name="Galeri" className="bd-card">
-                    <SimpleReactLightbox>
-                      <BusinessDetailGallery />
-                    </SimpleReactLightbox>
-                  </section>
-                  <section ref={ (ref) => this.backersRef=ref } name="Pendukung" className="bd-card">
-                    <div className="bd-card__content">
-                      <BusinessDetailBackers title="Nama Bisnis"  />
-                    </div>
-                  </section>
-                </div>
-                <div className="content__sidebar">
-                  <BusinessDetailForm scrollToBackers={this.scrollToBackers} />
-                </div>
-              </main>
-              <Recomendations />
-            </Fragment>
-          }
+          { isLoading ? this.renderLoadingDesktop() : this.renderDetailDesktop() }
           <Footer />
         </Desktop>
         <Mobile>
           <Navbar title="Detail Bisnis" />
-          { isLoading ?
-            <Fragment>
-              <main className="container--mobile">
-                <section name="Pengantar Bisnis" className="bd-card__content">
-                  <BusinessDetailIntro isLoading />
-                </section>
-                <section name="Tentang Bisnis" className="bd-card__content">
-                  <BusinessDetailAbout isLoading />
-                </section>
-                <hr className="divider" />
-                <section name="Cara Mendukung">
-                  <BusinessDetailHowTo isLoading  />
-                </section>
-                <hr className="divider" />
-                <section name="Galeri">
-                  <BusinessDetailGallery isLoading />
-                </section>
-                <hr className="divider" />
-                <section name="Pendukung" className="bd-card__content" style={{ marginBottom: '72px' }}>
-                  <BusinessDetailBackers isLoading />
-                </section>
-              </main>
-            </Fragment>
-            :
-            <Fragment>
-              <main className="container--mobile">
-                <section name="Pengantar Bisnis" className="bd-card__content">
-                  <BusinessDetailIntro 
-                    img="https://via.placeholder.com/120x120" 
-                    title="Nama Bisnis" 
-                    category="Kategori"
-                    location="Lokasi" />
-                </section>
-                <section name="Tentang Bisnis" className="bd-card__content">
-                  <BusinessDetailAbout
-                    title="Nama Bisnis" 
-                    desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type book."
-                    instagram="sylviestephanies"
-                    tokopedia="houseoforganix"
-                  />
-                </section>
-                <hr className="divider" />
-                <section name="Cara Mendukung">
-                  <BusinessDetailHowTo title="Nama Bisnis"  />
-                </section>
-                <hr className="divider" />
-                <section name="Galeri">
-                  <SimpleReactLightbox>
-                    <BusinessDetailGallery />
-                  </SimpleReactLightbox>
-                </section>
-                <hr className="divider" />
-                <section name="Pendukung" className="bd-card__content" style={{ marginBottom: '72px' }}>
-                  <BusinessDetailBackers title="Nama Bisnis"  />
-                </section>
-              </main>
-              <div className="bd-bottom">
-                <div className="bd-bottom__content">
-                  <button className={ClassNames('button button--main')} onClick={this.toggleForm}>Dukung Nama Bisnis</button>
-                </div>
-              </div>
-              <BottomSheet title="Dukung Nama Bisnis"
-                onClose={this.toggleForm}
-                display={showForm}
-              >
-                <div className="bd-bottom__form">
-                <BusinessDetailForm />
-                </div>
-              </BottomSheet>
-            </Fragment>
-          }
+          { isLoading ? this.renderLoadingMobile() : this.renderDetailMobile() }
         </Mobile>
       </Fragment>
     )
