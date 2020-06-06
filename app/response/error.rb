6f41a8169af352b691c6fdc0a8ecb1c932918853
sun <sunderip128@gmail.com>
2020-06-06 22:40:32 +0700

    Post Business API (#63)
    * [preview] init post business api
    
    * [preview] block invalid category and add postman
    
    * [preview] bugfix
    
    * [preview] missed error
    
    * [preview] nil url
    
    * slug regex
    
    * [preview] storeurl
    
    * [preview] bugfix

--- app/response/error.rb
+++ app/response/error.rb
@@ -1,9 +1,25 @@
+ERROR_ADMIN_AUTHENTICATION = 999
 ERROR_MISSING_REQUIRED_PARAMETER=1000
 ERROR_INVALID_PARAMETER = 1001
 ERROR_RESOURCE_NOT_FOUND = 1002
 ERROR_VALIDATION = 1003
 ERROR_DUPLICATE_ENTRY = 1004
 ERROR_INVALID_REQUEST_BODY = 1005
+ERROR_ASCII_EXPECTED = 1006
+ERROR_UNKNOWN_CATEGORY = 1007
+
+
+class AuthenticationError < StandardError
+  def initialize(msg="invalid token")
+    super
+  end
+end
+
+class InvalidEncodingError < StandardError
+  def initialize(msg="invalid encoding error")
+    super
+  end
+end
 
 class MissingParameterError < StandardError
   def initialize(msg="parameter missing")
@@ -13,6 +29,12 @@ end
 
 class ResourceNotFoundError < StandardError
   def initialize(msg="resource not found")
+    super
+  end
+end
+
+class InvalidInputError < StandardError
+  def initialize(msg="input invalid")
     super
   end
 end
