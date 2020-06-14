e744280444b3cba74d10e0fe2ea832fa2bb1905c
Devin Ryan Riota <devinryanriota@gmail.com>
2020-06-14 15:01:35 +0700

    add social links on business detail

--- src/routes/BusinessDetail/BusinessDetail.jsx
+++ src/routes/BusinessDetail/BusinessDetail.jsx
@@ -376,6 +376,7 @@ class BusinessDetail extends Component {
     const { businessDetail } = this.state
     let title = "Yuk Bantu Bisnis Online"
     let description = ""
+    const url = window.location.href
     if(typeof businessDetail.name !== 'undefined' && typeof businessDetail.description !== 'undefined') {
       title = businessDetail.name + " | Yuk Bantu Bisnis Online"
       description = businessDetail.description
@@ -388,6 +389,16 @@ class BusinessDetail extends Component {
         <meta name = "description" 
           content = { description }
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content= { url } />
+        <meta property="og:title" content= { title } />
+        <meta property="og:description" content= { description } />
+        <meta property="og:image" content= { businessDetail.thumbnailUrl } />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content= { url } />
+        <meta property="twitter:title" content= { title } />
+        <meta property="twitter:description" content= { description } />
+        <meta property="twitter:image" content= { businessDetail.thumbnailUrl } />
       </Helmet>
     )
   }
