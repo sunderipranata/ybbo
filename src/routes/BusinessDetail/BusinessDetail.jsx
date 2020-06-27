40d7a605e6f42a935e4e309c295d83b70d9882b0
Devin Ryan Riota <devinryanriota@gmail.com>
2020-06-27 16:47:43 +0700

    use meta from each page

--- src/routes/BusinessDetail/BusinessDetail.jsx
+++ src/routes/BusinessDetail/BusinessDetail.jsx
@@ -395,12 +395,12 @@ class BusinessDetail extends Component {
         <meta property="og:url" content= { url } />
         <meta property="og:title" content= { title } />
         <meta property="og:description" content= { description } />
-        <meta property="og:image" content= { businessDetail.thumbnailUrl } />
+        <meta property="og:image" content="https://i.ibb.co/1Lp3TXp/og-ybbo.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content= { url } />
         <meta property="twitter:title" content= { title } />
         <meta property="twitter:description" content= { description } />
-        <meta property="twitter:image" content= { businessDetail.thumbnailUrl } />
+        <meta property="twitter:image" content="https://i.ibb.co/1Lp3TXp/og-ybbo.jpg" />
       </Helmet>
     )
   }
