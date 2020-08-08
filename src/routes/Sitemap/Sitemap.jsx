327b02c6a355960d6f2515c5963824e1b4ce8b2b
Devin Ryan Riota <devinryanriota@gmail.com>
2020-08-08 17:05:12 +0700

    render directory

--- src/routes/Sitemap/Sitemap.jsx
+++ src/routes/Sitemap/Sitemap.jsx
@@ -13,7 +13,28 @@ import PageLabel from "../../utils/googleAnalytics/PageLabel"
 library.add(fab)
 
 class Sitemap extends Component {
-  componentDidMount() {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: 0,
+      data: []
+    }
+  }
+
+  componentWillMount = () => {
+    let page = this.props.match.params.page
+
+    if(typeof page === 'undefined' || page === null) {
+      page = 1
+    }
+
+    this.setState({
+      page: page
+    })
+  }
+
+  componentDidMount = () => {
     window.scrollTo({top: 0})
   }
 
@@ -30,6 +51,58 @@ class Sitemap extends Component {
       </Helmet>
     )
   }
+
+  renderDirectory = () => {
+    const { page, data } = this.state
+    let display = []
+
+    let totalPage = 2
+    const baseUrl = '/sitemap/'
+    const activeClassName = 'active'
+    const defaultClassName = ''
+    //iterate through how many page
+    for(var i = 1; i <= totalPage; i++) {
+      const className = parseInt(page) === i ? activeClassName : defaultClassName
+
+      display.push(
+        <li className = { className }>
+          <a href = { baseUrl + i }>{ i }</a>
+        </li>
+      )
+    }
+
+    return (
+      <div className = "directory-list">
+        <table>
+          <tr>
+            <td>Directory: </td>
+            <td>
+              <ul>
+                { display }
+              </ul>
+            </td>
+          </tr>
+        </table>
+      </div>
+    )
+  }
+
+  renderData = () => {
+    const { data } = this.state
+    let display = []
+
+    data.forEach(d => {
+
+
+    })
+
+    return (
+      <div>
+        { display }
+      </div>
+    )
+
+  }
   
   render() {
     return (
@@ -37,8 +110,9 @@ class Sitemap extends Component {
         { this.renderHelmet() }
         <article>
         <Header pageLabel={PageLabel.HEADER}/>
-        <main className="container container__about clearfix">
+        <main className="container container__sitemap clearfix">
           <h1 className="title">Sitemap</h1>
+          { this.renderDirectory() }
           <p className="desc">
             Kami tim Software Engineers yang mencoba untuk membantu UMKM Indonesia melalui teknologi.{' '}
             Inovasi ini ditujukan untuk membantu ekonomi yang sedang melambat karena pandemi COVID-19.
